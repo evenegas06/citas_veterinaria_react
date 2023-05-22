654fdabf7acc443d2b95da6503a9bd4e929a3d9b
Eduardo Venegas <evenegas06@hotmail.com>
2023-05-21 23:05:29 -0600

    create edit and delete buttons

--- src/components/PatientItem.jsx
+++ src/components/PatientItem.jsx
@@ -1,7 +1,7 @@
 /* eslint-disable react/prop-types */
 const PatientItem = ({ patient }) => {
-    const {pet, owner, email, date, symptoms} = patient;
-    
+    const { pet, owner, email, date, symptoms } = patient;
+
     return (
         <li className="mx-3 mb-3 bg-white shadow-md px-5 py-10 rounded-xl">
             <p className="font-bold mb-3 text-gray-700 uppercase">
@@ -38,6 +38,22 @@ const PatientItem = ({ patient }) => {
                     &nbsp;{symptoms}
                 </span>
             </p>
+
+            <div className="flex justify-around mt-10">
+                <button
+                    type="button"
+                    className="py-2 px-10 bg-indigo-600 hover:bg-indigo-700 text-white font-bold uppercase rounded-lg"
+                >
+                    Editar
+                </button>
+                
+                <button
+                    type="button"
+                    className="py-2 px-10 bg-red-600 hover:bg-red-700 text-white font-bold uppercase rounded-lg"
+                >
+                    Eliminar
+                </button>
+            </div>
         </li>
     );
 };
--- src/components/PatientsList.jsx
+++ src/components/PatientsList.jsx
@@ -4,27 +4,44 @@ import PatientItem from "./PatientItem";
 const PatientsList = ({ patient_list }) => {
     return (
         <div className="md:w-1/2 lg:w-3/5">
-            <h2 className="font-black text-3xl text-center">
-                Listado Pacientes
-            </h2>
+            {patient_list.length ? (
+                <>
+                    <h2 className="font-black text-3xl text-center">
+                        Listado Pacientes
+                    </h2>
 
-            <p className="text-xl mt-5 mb-7 text-center">
-                Administra tus
-                <span className="text-indigo-600 font-bold">
-                    &nbsp;Pacientes y Citas
-                </span>
-            </p>
+                    <p className="text-xl mt-5 mb-7 text-center">
+                        Administra tus
+                        <span className="text-indigo-600 font-bold">
+                            &nbsp;Pacientes y Citas
+                        </span>
+                    </p>
 
-            <ul className="md:h-screen overflow-y-scroll mx-5">
-                {patient_list.map((patient) => {
-                    return (
-                        <PatientItem
-                            key={patient.id}
-                            patient={patient} 
-                        />
-                    );
-                })}
-            </ul>
+                    <ul className="md:h-screen overflow-y-scroll mx-5">
+                        {patient_list.map((patient) => {
+                            return (
+                                <PatientItem
+                                    key={patient.id}
+                                    patient={patient}
+                                />
+                            );
+                        })}
+                    </ul>
+                </>
+            ) : (
+                <>
+                    <h2 className="font-black text-3xl text-center">
+                        No hay pacientes aun
+                    </h2>
+
+                    <p className="text-xl mt-5 mb-7 text-center">
+                        Completa el formulario
+                        <span className="text-indigo-600 font-bold">
+                            &nbsp;y aparecerán aquí
+                        </span>
+                    </p>
+                </>
+            )}
         </div>
     );
 };
